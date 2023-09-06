21c9d51b13e4960f9bf8e457ed78627f2adee773
Drake Buentello <dbuentello211607@gmail.com>
2023-09-06 07:42:17 -0500

    added animation controller for handling random animation on core, 'laugh' 'blink' and 'spin'

--- Assets/Scripts/Core/CoreController.cs
+++ Assets/Scripts/Core/CoreController.cs
@@ -1,16 +1,33 @@
+using System.Collections;
 using UnityEngine;
 
 public class CoreController : MonoBehaviour
 {
+    [SerializeField]
+    private Animator animator;
+    [SerializeField]
+    private int numRandomAnimations;
+    [SerializeField]
+    private float randomAnimationTime;
+    
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        StartCoroutine("RandomAnimation");
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator RandomAnimation()
     {
-        
+        while (true)
+        {
+            int randInt = Random.Range(1, numRandomAnimations + 1);
+            animator.SetInteger("randomAnimation", randInt);
+
+             yield return new WaitForSeconds(1f);
+            // Zero resets back to idle animation
+            animator.SetInteger("randomAnimation", 0);
+
+            yield return new WaitForSeconds(randomAnimationTime);
+        }
     }
 }
